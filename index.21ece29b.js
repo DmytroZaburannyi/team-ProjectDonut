8d7530d3e1e3ebc0a3773a6ef776255c807e43e1
DmytroZaburannyi <113027547+DmytroZaburannyi@users.noreply.github.com>
2022-11-19 15:19:40 +0000

    Deploying to gh-pages from @ DmytroZaburannyi/project-demo@0cc4387f8b74109b7bb9188c4f817fe28276262a 🚀

---
+++ index.21ece29b.js
@@ -0,0 +1,2 @@
+var btn=document.querySelector(".read-more-btn"),doptext=document.querySelector(".about-wrapper__text--more");btn.addEventListener("click",(function(){doptext.classList.toggle("is-open"),doptext.classList.contains("is-open")?btn.textContent="Less":btn.textContent="Read more"}));
+//# sourceMappingURL=index.21ece29b.js.map
